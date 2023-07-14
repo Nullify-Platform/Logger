9417b2b1133c09dc7a7e09fc8f9ac8189ff5de64
Tim Thacker <tim.thacker@nullify.cloud>
2023-07-14 11:28:45 +1000

    add generic http request logging

--- pkg/logger/http.go
+++ pkg/logger/http.go
@@ -6,6 +6,73 @@ import (
 	"go.uber.org/zap"
 )
 
+func NewLoggingTransport(baseTransport http.RoundTripper) http.RoundTripper {
+	return &LoggingTransport{
+		baseTransport: baseTransport,
+	}
+}
+
+func NewLoggingTransportWithLogger(baseTransport http.RoundTripper, logger Logger) http.RoundTripper {
+	return &LoggingTransport{
+		logger:        logger,
+		baseTransport: baseTransport,
+	}
+}
+
+type LoggingTransport struct {
+	logger        Logger
+	baseTransport http.RoundTripper
+}
+
+func (t *LoggingTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	res, err := t.baseTransport.RoundTrip(req)
+	if err != nil {
+		return res, err
+	}
+
+	reqHeaders := []string{}
+	for header, values := range req.Header {
+		for _, value := range values {
+			reqHeaders = append(reqHeaders, header+": "+value)
+		}
+	}
+
+	resHeaders := []string{}
+	for header, values := range res.Header {
+		for _, value := range values {
+			resHeaders = append(resHeaders, header+": "+value)
+		}
+	}
+
+	if t.logger == nil {
+		Info(
+			"request summary",
+			Any("requestSummary", &HTTPRequestMetadata{
+				Service:         "cognito",
+				Method:          req.Method,
+				URL:             req.URL.String(),
+				StatusCode:      res.StatusCode,
+				RequestHeaders:  reqHeaders,
+				ResponseHeaders: resHeaders,
+			}),
+		)
+	} else {
+		t.logger.Info(
+			"request summary",
+			Any("requestSummary", &HTTPRequestMetadata{
+				Service:         "cognito",
+				Method:          req.Method,
+				URL:             req.URL.String(),
+				StatusCode:      res.StatusCode,
+				RequestHeaders:  reqHeaders,
+				ResponseHeaders: resHeaders,
+			}),
+		)
+	}
+
+	return res, err
+}
+
 type HTTPRequestMetadata struct {
 	Service         string   `json:"service"`
 	Method          string   `json:"method"`
