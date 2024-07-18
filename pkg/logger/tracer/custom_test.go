b46848113c0cca31339408a1b225a92ecc54008d
Tim Thacker <123459940+tim-thacker-nullify@users.noreply.github.com>
2024-07-18 15:08:14 +1000

    add trace propagation unit tests (#81)

---
+++ pkg/logger/tracer/custom_test.go
@@ -0,0 +1,40 @@
+package tracer
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+func TestCustomAttributeCarrier(t *testing.T) {
+	ctx := context.Background()
+	tc := propagation.TraceContext{}
+	tp := sdktrace.NewTracerProvider()
+	ctx = NewContext(ctx, tp, "test-logger-tracer")
+
+	// extract
+
+	extractAttributes := map[string]string{
+		"traceparent": "00-adabf450d7a37f3a7b708aaaffffe150-95f2f55992ea2f1e-01",
+	}
+
+	ctx = tc.Extract(ctx, newCustomEventMessageCarrier(extractAttributes))
+	ctx, span := FromContext(ctx).Start(ctx, "TestCustomAttributeCarrier")
+
+	assert.Equal(t, "adabf450d7a37f3a7b708aaaffffe150", span.SpanContext().TraceID().String())
+
+	// inject
+	injectAttributes := map[string]string{}
+
+	tc.Inject(ctx, newCustomMessageCarrier(injectAttributes))
+
+	expectedInjectAttributes := map[string]string{
+		"traceparent": fmt.Sprintf("00-adabf450d7a37f3a7b708aaaffffe150-%s-01", span.SpanContext().SpanID().String()),
+	}
+
+	assert.Equal(t, expectedInjectAttributes, injectAttributes)
+}
