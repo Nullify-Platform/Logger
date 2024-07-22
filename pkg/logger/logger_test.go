1b8a5adc4d3927845eca7df4223f32171c526499
Nicholas Albion <nicholas.albion@nullify.cloud>
2024-07-22 12:56:55 +1000

    go fmt

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -12,7 +12,7 @@ func TestFormatLogsURL(t *testing.T) {
 	logStreamName := "2024/07/22/[$LATEST]83b2196b0fb5466e9e71f9e6c9eceab8"
 
 	expectedURL := "https://ap-southeast-2.console.aws.amazon.com/cloudwatch/home?region=ap-southeast-2#logsV2:log-groups/log-group/" +
-		"$252Faws$252Flambda$252Ffoxsports-the-force-pullrequest/log-events/" + 
+		"$252Faws$252Flambda$252Ffoxsports-the-force-pullrequest/log-events/" +
 		"2024$252F07$252F22$252F$255B$2524LATEST$255D83b2196b0fb5466e9e71f9e6c9eceab8"
 
 	result := formatLogsURL(region, logGroupName, logStreamName)
