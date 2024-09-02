1356fa42a299993f01331a3ed0a27110165d62bf
Nicholas Albion <nicholas.albion@nullify.cloud>
2024-09-03 09:40:19 +1000

    NUL-1132 AddSentryTag()

--- pkg/logger/configure.go
+++ pkg/logger/configure.go
@@ -33,6 +33,8 @@ import (
 // e.g. -ldflags "-X 'github.com/nullify-platform/logger/pkg/logger.Version=$(VERSION)'"
 var Version = "0.0.0"
 
+var extraTags = map[string]string{}
+
 // ConfigureDevelopmentLogger configures a development logger which is more human readable instead of JSON
 func ConfigureDevelopmentLogger(ctx context.Context, level string, syncs ...io.Writer) (context.Context, error) {
 	// configure level
@@ -284,6 +286,10 @@ func AddECSTagsToSentryEvents(ctx context.Context, awsConfig aws.Config) error {
 	return nil
 }
 
+func AddSentryTag(key, value string) {
+	extraTags[key] = value
+}
+
 func addTagsToSentryEvents(functionName string, region string, tags map[string]string) {
 	zap.L().Info("adding tags to sentry events", zap.String("functionName", functionName), zap.String("region", region), zap.Any("tags", tags))
 
@@ -307,6 +313,10 @@ func addTagsToSentryEvents(functionName string, region string, tags map[string]s
 		event.Tags["environment"] = tags["Environment"]
 		event.Tags["tenant"] = tags["Tenant"]
 		event.Tags["service"] = tags["Service"]
+
+		for k, v := range extraTags {
+			event.Tags[k] = v
+		}
 
 		return event
 	})
